b7a894bce8b9fac925a0588d6da18d7c9cd51cb8
linjun-ddddd <654548718@qq.com>
2020-10-10 18:18:51 +0800

    feat: init code

--- src/backend/core/src/main/kotlin/com/tencent/devops/api/PipelineTaskResourceApi.kt
+++ src/backend/core/src/main/kotlin/com/tencent/devops/api/PipelineTaskResourceApi.kt
@@ -2,9 +2,11 @@ package com.tencent.devops.api
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.tencent.bk.devops.atom.api.BaseApi
+import com.tencent.bk.devops.atom.common.Constants
 import com.tencent.bk.devops.plugin.pojo.Result
 import com.tencent.bk.devops.plugin.utils.JsonUtil
 import com.tencent.devops.pojo.process.PipelineBuildTaskInfo
+import java.io.File
 
 class PipelineTaskResourceApi : BaseApi() {
 
